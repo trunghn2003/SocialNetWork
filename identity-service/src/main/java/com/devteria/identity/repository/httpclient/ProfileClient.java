491f7ee3da1dcb5e0d317ca01fc496b574b4324b
trunghn2003 <134114519+trunghn2003@users.noreply.github.com>
2024-07-08 10:53:27 +0700

    add configuration

--- identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -1,5 +1,6 @@
 package com.devteria.identity.repository.httpclient;
 
+import com.devteria.identity.configuration.AuthenticationRequestInterceptor;
 import com.devteria.identity.dto.request.ProfileCreationRequest;
 import com.devteria.identity.dto.response.UserProfileResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.awt.*;
 
-@FeignClient(name = "profile-service", url = "${app.profile.url}")
+@FeignClient(name = "profile-service", url = "${app.profile.url}", configuration = AuthenticationRequestInterceptor.class)
 public interface ProfileClient {
     @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserProfileResponse createUserProfile(@RequestBody ProfileCreationRequest request);
+    UserProfileResponse createUserProfile( @RequestBody ProfileCreationRequest request);
 
 }
